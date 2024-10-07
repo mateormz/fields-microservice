1330efd99d76a742de6ce7612ca08f9de9cef0b4
Joel Miguel <joel.miguel@utec.edu.pe>
2024-10-06 23:50:00 -0500

    avance

--- src/main/java/org/example/fieldsmanagement/comments/application/CommentsController.java
+++ src/main/java/org/example/fieldsmanagement/comments/application/CommentsController.java
@@ -9,19 +9,24 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/comments")
 public class CommentsController {
 
     @Autowired
     private CommentsService commentsService;
 
-    @PostMapping("/create")
+    @GetMapping("/")
+    public ResponseEntity<String> welcomeApi() {
+        String welcome = "welcome to fieldsmanagement";
+        return  ResponseEntity.ok(welcome);
+    }
+
+    @PostMapping("/comments/create")
     public ResponseEntity<Comments> createComment(@RequestBody Comments comment) {
         Comments savedComment = commentsService.createComment(comment);
         return ResponseEntity.ok(savedComment);
     }
 
-    @GetMapping("/field/{fieldId}")
+    @GetMapping("/comments/field/{fieldId}")
     public ResponseEntity<List<Comments>> getCommentsByFieldId(@PathVariable Long fieldId) {
         List<Comments> comments = commentsService.getCommentsByFieldId(fieldId);
         return ResponseEntity.ok(comments);
